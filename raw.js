0633907f55424686ce342fc3c55364f76c36cbcd
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-01-09 12:46:18 +0100

    Update raw.js

--- raw.js
+++ raw.js
@@ -4,3 +4,34 @@ CreateRawObject({
 	'a':{value:1},//{value:undefined, writable:false, enumerable:false, configurable:false}
 	'b':{value:2}
  });
+ 
+ 
+ 
+ /*
+  var cssOf=function(aDoc){
+	var found=(function(){
+	 var a=CreateRawArray,f=function(){
+		var c=CreateRawObject,o={},m=arguments,i=m.length+0,e;
+		while(i--){e=m[i];o[e[0]]={value:e[1]};};
+		return c(o);
+	 };
+	 return f([
+		'sheets',f(
+		 ['external',f(
+			['link',a()],
+			['imported',f(
+			 ['fromLinked',a()],
+			 ['fromImported',a()],
+			 ['fromEmbedded',a()]
+			)]
+		 )],
+		 ['internal',f(
+			['embedded',a()],
+			['inline',a()]
+		)])
+	 ]);
+	})();
+	return found;
+ },
+ cssfounded=cssOf($d);
+ */
